676e63eb6e4852fb697c38254533702bdf1c4e53
Shubham2rock <93591468+Shubham2rock@users.noreply.github.com>
2021-12-10 12:12:11 +0530

    Add files via upload

---
+++ node_tut/13-async-eventloop-util.js
@@ -0,0 +1,28 @@
+const { readFile, writeFile, read } = require('fs')
+const util = require('util')
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
+
+const start = async () => {
+  try {
+    const first = await readFilePromise('./content/first.txt', 'utf-8')
+    const second = await readFilePromise('./content/second.txt', 'utf-8')
+    console.log(first, second)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf-8', (err, data) => {
+//       if (err) {
+//         reject(err)
+//       } else {
+//         resolve(data)
+//       }
+//     })
+//   })
+// }
